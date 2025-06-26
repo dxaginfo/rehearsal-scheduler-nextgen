71131572899b4fbb8a067c679769bb53f494f668
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-26 04:18:48 -0300

    Initial frontend code (React entrypoint)

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Rehearsal Scheduler</h1>
+        <p>Welcome. Schedule rehearsals, manage attendance, and sync calendars easily.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
